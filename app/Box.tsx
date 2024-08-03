9b0d18a79929a147c77931908f3d63482ec01f7f
Ariana I <git@arianaivan.com>
2024-08-02 22:00:54 -0700

    box design: shadows

--- app/Box.tsx
+++ app/Box.tsx
@@ -10,10 +10,13 @@ export const Box = ({
   children: string | JSX.Element | JSX.Element[]
 }) => {
   return (
-    <div style={{ borderColor: color }} className="border">
+    <div
+      style={{ borderColor: color }}
+      className="border rounded shadow-md hover:shadow-xl"
+    >
       <h2
         style={{ backgroundColor: color }}
-        className=" text-center text-white py-1.5 font-bold"
+        className=" text-center text-[#FFF8F6] py-1.5 font-bold"
       >
         {title}
       </h2>
