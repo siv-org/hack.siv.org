1bfc79bd4b09f0a26a96e4bd098cd5479833df03
David Ernst <git@dsernst.com>
2024-08-04 01:13:56 -0700

    Countdown to 10am Sunday Aug 11

--- app/Countdown.tsx
+++ app/Countdown.tsx
@@ -1,6 +1,13 @@
+'use client'
+
+import ReactCountdown from 'react-countdown'
+
 export const Countdown = () => (
   <div className="uppercase font-semibold">
     Submissions close in
-    <div className="text-center text-red-600 mt-1">7 days</div>
+    <ReactCountdown
+      className="block text-center text-red-500 text-xl mt-1 font-mono"
+      date={new Date('2024-08-11T10:00:00-07:00')}
+    />
   </div>
 )
