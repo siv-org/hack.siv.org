649db41fe1558b97409e89db79a2d198860eab49
David Ernst <git@dsernst.com>
2024-08-05 00:30:02 -0700

    register-email: Show 'Submitting...' loading state

--- app/register.tsx
+++ app/register.tsx
@@ -5,6 +5,7 @@ import { MdOutlineNotificationsActive } from 'react-icons/md'
 
 export const Register = () => {
   const [email, setEmail] = useState('')
+  const [submitting, setSubmitting] = useState(false)
   const [success, setSuccess] = useState(false)
 
   return (
@@ -14,7 +15,7 @@ export const Register = () => {
         <div className="pl-3 text-left">
           <div className="text-xl">
             {!success ? (
-              'Signup for Invites'
+              'Signup for invites'
             ) : (
               <>
                 Signup successful<span className="ml-3">✅</span>
@@ -33,17 +34,20 @@ export const Register = () => {
         onSubmit={async (event: React.FormEvent) => {
           event.preventDefault()
 
-          const body = JSON.stringify({ email })
-          console.log({ body })
-          await fetch('https://siv.org/api/hack-siv/register', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Accept: 'application/json',
-            },
-            body,
-          })
-          setSuccess(true)
+          setSubmitting(true)
+          const response = await fetch(
+            'https://siv.org/api/hack-siv/register',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+              body: JSON.stringify({ email }),
+            }
+          )
+          setSubmitting(false)
+          if (response.ok) setSuccess(true)
         }}
         className="flex"
       >
@@ -51,18 +55,19 @@ export const Register = () => {
           type="email"
           placeholder="you@email.com"
           value={email}
+          required
           onChange={(e) => {
             setEmail(e.target.value)
             setSuccess(false)
           }}
-          className="p-3 rounded-bl-md border-2 border-r-0 border-gray-300/30 bg-white/10 w-full"
+          className="p-3 pl-4 rounded-bl-md border-2 border-r-0 border-gray-300/30 bg-white/10 w-full"
         />
         <button
           type="submit"
           disabled={!email || success}
-          className="border-2 border-gray-300/30 hover:bg-gray-800 text-white/80 p-2 rounded-br-md disabled:cursor-not-allowed disabled:opacity-80"
+          className="w-48 border-2 border-gray-300/30 hover:bg-gray-800 text-white/80 p-2 rounded-br-md disabled:cursor-not-allowed disabled:opacity-80"
         >
-          Submit
+          Submit{submitting ? 'ting...' : success && 'ted.'}
         </button>
       </form>
     </div>
