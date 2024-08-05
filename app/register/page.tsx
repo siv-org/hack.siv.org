2228b8d8455b46f465449afe6d4e146be38aeda4
David Ernst <git@dsernst.com>
2024-08-05 01:58:39 -0700

    Fix /register import miss

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import { Register } from '../RegisterWidget'
+import { RegisterWidget } from '../RegisterWidget'
 
 export default function RegisterPage() {
   return (
     <main className="flex min-h-screen max-w-3xl mx-auto flex-col items-center p-6 pt-10 sm:p-24 space-y-12 sm:text-lg">
       <h3 className="text-xl">Register for HACK SIV</h3>
-      <Register />
+      <RegisterWidget />
 
       <Link
         className="underline text-blue-400 text-opacity-60 hover:text-opacity-100"
