d93195d631e52ce3368efa410b032819e34ba962
David Ernst <git@dsernst.com>
2024-08-06 12:17:35 -0700

    rules: space-y-6 every line

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,7 +3,7 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-3xl font-semibold border-b border-white/50 pb-3 mb-3">
+      <h1 className="text-3xl font-semibold border-b border-white/30 pb-3 mb-5">
         {children}
       </h1>
     ),
