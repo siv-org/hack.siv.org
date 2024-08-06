e2b76e698f376e069093c3874f9a054e6c9dbc23
David Ernst <git@dsernst.com>
2024-08-06 12:10:49 -0700

    mdx: Custom h1 styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,12 @@
 import type { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return { ...components }
+  return {
+    h1: ({ children }) => (
+      <h1 className="text-3xl font-semibold border-b border-white/50 pb-3 mb-3">
+        {children}
+      </h1>
+    ),
+    ...components,
+  }
 }
