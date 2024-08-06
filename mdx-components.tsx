a25048a5c6f2a6d48ea7bab8b2fb114fcd8bb045
David Ernst <git@dsernst.com>
2024-08-06 14:53:52 -0700

    mdx: Support custom table col alignment

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -43,8 +43,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     th: ({ children }) => (
       <th className="border border-gray-700 px-2 py-2">{children}</th>
     ),
-    td: ({ children }) => (
-      <td className="border border-gray-700 px-2 py-2">{children}</td>
+    td: ({ children, style }) => (
+      <td style={style} className="border border-gray-700 px-2 py-2">
+        {children}
+      </td>
     ),
     blockquote: ({ children }) => (
       <blockquote className="border-l-4 border-gray-700 pl-4 text-white/60 space-y-5">
