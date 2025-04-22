bbfd991c2dad1b826c8bde5d355289eae1f53d8a
Ariana I <git@arianaivan.com>
2025-04-21 23:24:28 -0700

    Report: Only show top margin when x-margins visible

--- app/reports/layout.tsx
+++ app/reports/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="bg-gray-300 py-8">
+    <div className="bg-gray-300 lg:py-8">
       <div className="p-8 max-w-5xl mx-auto space-y-5 bg-white text-black/90 rounded">
         {children}
       </div>
