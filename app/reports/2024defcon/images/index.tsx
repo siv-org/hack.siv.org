5e5e27bf3a0aed0ac794233511508c7d7dbfc68b
Ariana I <git@arianaivan.com>
2025-04-22 13:38:09 -0700

    report: Add caption to <AwardsProcessImage />

--- app/reports/2024defcon/images/index.tsx
+++ app/reports/2024defcon/images/index.tsx
@@ -2,11 +2,18 @@ import Image from 'next/image'
 
 import AwardsProcessScreenshot from './awards-process.png'
 
-export const AwardsProcessImage = () => (
-  <Image
-    src={AwardsProcessScreenshot}
-    alt="Awards Process"
-    width={600}
-    className="rounded-xl mx-auto"
-  />
+export const AwardsProcessImage = ({
+  children,
+}: {
+  children?: React.ReactNode
+}) => (
+  <div className="space-y-2">
+    <Image
+      src={AwardsProcessScreenshot}
+      alt="Awards Process"
+      width={600}
+      className="rounded-xl mx-auto"
+    />
+    {children && <div className="text-center text-base italic">{children}</div>}
+  </div>
 )
