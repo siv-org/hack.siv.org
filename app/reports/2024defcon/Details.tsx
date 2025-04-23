644ae30a3b53bf8bc6d408e5cb7e6f6d2b14794c
Ariana I <git@arianaivan.com>
2025-04-22 20:40:48 -0700

    report: custom css for nested details

--- app/reports/2024defcon/Details.tsx
+++ app/reports/2024defcon/Details.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export const Details = ({
   children,
   title,
@@ -8,7 +10,7 @@ export const Details = ({
   readTime: number
 }) => {
   return (
-    <details>
+    <details className="Details">
       <summary className="text-[22px] font-medium cursor-pointer text-[#111827] leading-[1.5] mb-5">
         {title}{' '}
         <span className="text-[16.5px] opacity-50 font-normal ml-2 text-[#6b7280]">
@@ -16,6 +18,17 @@ export const Details = ({
         </span>
       </summary>
       <div className="pl-3">{children}</div>
+
+      {/* Custom css for nested details */}
+      <style jsx global>{`
+        .Details .Details {
+          margin-left: 15px;
+        }
+
+        .Details .Details summary {
+          font-size: 19px !important;
+        }
+      `}</style>
     </details>
   )
 }
