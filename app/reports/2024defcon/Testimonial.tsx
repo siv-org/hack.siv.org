cf7a630edfcebdbd65a65bfa23a113ee1a22b8fd
Ariana I <git@arianaivan.com>
2025-04-22 18:17:33 -0700

    report: Testimonal styling of author

--- app/reports/2024defcon/Testimonial.tsx
+++ app/reports/2024defcon/Testimonial.tsx
@@ -12,8 +12,8 @@ export const Testimonial = ({
   return (
     <div className="bg-gray-50 p-12 rounded-sm shadow-lg mb-6 border-l-4 border-blue-500">
       {children}
-      <div className="text-right mt-4">
-        <div className="text-lg italic">— {author}</div>
+      <div className=" text-base text-right mt-4 text-slate-500 italic">
+        <div>— {author}</div>
         {secondLine && <div className="text-base italic">{secondLine}</div>}
         {thirdLine && <div className="text-base italic">{thirdLine}</div>}
       </div>
