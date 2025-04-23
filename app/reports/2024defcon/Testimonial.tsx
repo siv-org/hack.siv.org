8672536916de4ea9c74b7b3cadedebc02cc45731
Ariana I <git@arianaivan.com>
2025-04-22 18:38:21 -0700

    report: Cleanup testimonial author stylng

--- app/reports/2024defcon/Testimonial.tsx
+++ app/reports/2024defcon/Testimonial.tsx
@@ -12,10 +12,10 @@ export const Testimonial = ({
   return (
     <div className="bg-gray-50 p-12 rounded-sm shadow-lg mb-6 border-l-4 border-blue-500">
       {children}
-      <div className=" text-base text-right mt-4 text-slate-500 italic">
+      <div className="text-base text-right mt-4 text-slate-500 italic">
         <div>— {author}</div>
-        {secondLine && <div className="text-base italic">{secondLine}</div>}
-        {thirdLine && <div className="text-base italic">{thirdLine}</div>}
+        {secondLine && <div>{secondLine}</div>}
+        {thirdLine && <div>{thirdLine}</div>}
       </div>
     </div>
   )
