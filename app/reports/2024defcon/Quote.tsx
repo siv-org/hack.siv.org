1d37e3b2f73e35c67f1ad15dee44010abedbdbf9
Ariana I <git@arianaivan.com>
2025-04-23 18:49:35 -0700

    report: add end quote and style for it

---
+++ app/reports/2024defcon/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = ({
+  author,
+  children,
+}: {
+  author: string
+  children: React.ReactNode
+}) => (
+  <div className="font-light text-center text-base italic text-slate-800 my-8 relative max-w-xl mx-auto px-6">
+    {children}
+    <span className="text-sm">{author}</span>
+
+    <div className="absolute -top-1 text-4xl -left-2 text-slate-300">
+      &ldquo;
+    </div>
+    <div className="absolute bottom-2 text-4xl -right-2 text-slate-300">
+      &rdquo;
+    </div>
+  </div>
+)
