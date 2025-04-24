4c64d91ec37fcd46e3090505123da5da9fbebfb8
Ariana I <git@arianaivan.com>
2025-04-23 20:28:07 -0700

    report: Keep <Details> reading time from line-wrapping

--- app/reports/2024defcon/Details.tsx
+++ app/reports/2024defcon/Details.tsx
@@ -14,7 +14,7 @@ export const Details = ({
       <summary className="text-[22px] font-medium cursor-pointer text-[#111827] leading-[1.5] mb-5">
         {title}{' '}
         <span className="text-[16.5px] opacity-50 font-normal ml-2 text-[#6b7280]">
-          ({readTime} min read)
+          ({readTime}&nbsp;min&nbsp;read)
         </span>
       </summary>
       <div className="pl-3 pb-6">{children}</div>
