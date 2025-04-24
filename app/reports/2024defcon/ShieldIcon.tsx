985e0923339630183c484dbd0e7f59e97203302d
Ariana I <git@arianaivan.com>
2025-04-23 18:22:20 -0700

    report: styling Contribute to HACK SIV

---
+++ app/reports/2024defcon/ShieldIcon.tsx
@@ -0,0 +1,16 @@
+export const ShieldIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="18"
+    height="18"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="text-[#0066cc] ml-2"
+  >
+    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+  </svg>
+)
