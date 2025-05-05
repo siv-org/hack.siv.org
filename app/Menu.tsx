1689219fab5da11585eb5950003d9c553c0def13
Ariana I <git@arianaivan.com>
2025-05-05 16:50:55 -0700

    Styling Menu

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -1,29 +1,31 @@
 import Link from 'next/link'
 
+const links = [
+  { href: '/', label: 'HACK SIV' },
+  { href: '/redvblue', label: 'Red vs Blue' },
+  { href: '/2024defcon', label: '2024 DEFCON' },
+  { href: '/reports/2024defcon', label: 'Reports' },
+]
+
 export const Menu = () => {
   return (
-    <nav className="p-4 text-white">
-      <ul className="flex gap-4">
-        <li>
-          <Link href="/" className="hover:text-blue-500">
+    <nav className="p-6 text-white">
+      <ul className="flex flex-wrap justify-center gap-x-8 gap-y-4">
+        <li className="w-full text-center tracking-wide text-xl">
+          <Link href="/" className="block hover:text-green-800 transition">
             HACK SIV
           </Link>
         </li>
-        <li>
-          <Link href="/redvblue" className="hover:text-blue-500">
-            Red vs Blue
-          </Link>
-        </li>
-        <li>
-          <Link href="/2024defcon" className="hover:text-blue-500">
-            2024 DEFCON
-          </Link>
-        </li>
-        <li>
-          <Link href="/reports/2024defcon" className="hover:text-blue-500">
-            Reports
-          </Link>
-        </li>
+        {links.slice(1).map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              href={href}
+              className="block hover:text-green-500 hover:underline transition text-lg"
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   )
