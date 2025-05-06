30766c8ddc30e9214fc72341635ffa77d1842072
Ariana I <git@arianaivan.com>
2025-05-05 18:18:19 -0700

    menu: change order to look more centered

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 const links = [
   { href: '/', label: 'HACK SIV' },
   { href: '/redvblue', label: 'Host Challenge' },
-  { href: '/2024defcon', label: '2024 DEFCON' },
   { href: '/reports/2024defcon', label: 'Reports' },
+  { href: '/2024defcon', label: '2024 DEFCON' },
 ]
 
 export const Menu = () => {
@@ -17,7 +17,7 @@ export const Menu = () => {
           </Link>
         </li>
         {links.slice(1).map(({ href, label }) => (
-          <li key={href}>
+          <li key={href} className="text-center">
             <Link
               href={href}
               className="block hover:text-green-500 hover:underline transition text-sm sm:text-lg"
