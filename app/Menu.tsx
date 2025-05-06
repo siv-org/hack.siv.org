42369a2da6e80e02baeb6a028cb3c7a93125547b
Ariana I <git@arianaivan.com>
2025-05-05 17:58:44 -0700

    landingpage: menu RedvBlue -> Host Challange

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const links = [
   { href: '/', label: 'HACK SIV' },
-  { href: '/redvblue', label: 'Red vs Blue' },
+  { href: '/redvblue', label: 'Host Challenge' },
   { href: '/2024defcon', label: '2024 DEFCON' },
   { href: '/reports/2024defcon', label: 'Reports' },
 ]
