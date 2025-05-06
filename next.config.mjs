483917857f5e56e9697bde76fff1fc3112d3fc65
David Ernst <git@dsernst.com>
2025-05-05 20:22:22 -0700

    Set up redirect from /rules -> /2024defcon/rules

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async redirects() {
+    return [
+      {
+        source: '/rules',
+        destination: '/2024defcon/rules',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 const withMDX = createMDX({
