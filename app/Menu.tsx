12c01231150315df91c5656b810ecb4535d83642
Ariana I <git@arianaivan.com>
2025-05-05 19:11:17 -0700

    menu: Tweak link colors

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -1,26 +1,46 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const links = [
-  { href: '/', label: 'HACK SIV' },
   { href: '/redvblue', label: 'Host Challenge' },
   { href: '/reports/2024defcon', label: 'Reports' },
   { href: '/2024defcon', label: '2024 DEFCON' },
 ]
 
+const activeLinkColor =
+  'bg-gradient-to-r from-blue-400 to-teal-400 bg-clip-text text-transparent'
+const hoverLinkColor = 'hover:text-sky-400'
+
 export const Menu = () => {
+  const pathname = usePathname()
+
   return (
     <nav className="p-6 text-white bg-[#114852] shadow-md shadow-green-900">
-      <ul className="flex flex-wrap justify-center gap-x-8 gap-y-4">
-        <li className="w-full text-center tracking-wide text-xl font-bold">
-          <Link href="/" className="block hover:text-green-500 transition">
+      {/* Top row */}
+      <div className="text-center tracking-wide text-xl w-full mb-4">
+        <span className="inline-block">
+          <Link
+            href="/"
+            className={`font-bold ${
+              pathname === '/' ? activeLinkColor : hoverLinkColor
+            }`}
+          >
             HACK SIV
           </Link>
-        </li>
-        {links.slice(1).map(({ href, label }) => (
+        </span>
+      </div>
+
+      {/* Bottom row */}
+      <ul className="flex justify-center gap-x-8">
+        {links.map(({ href, label }) => (
           <li key={href} className="text-center">
             <Link
               href={href}
-              className="block hover:text-green-500 hover:underline transition text-sm sm:text-lg"
+              className={`text-sm sm:text-lg ${
+                pathname === href ? activeLinkColor : hoverLinkColor
+              }`}
             >
               {label}
             </Link>
