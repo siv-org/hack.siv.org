c433b911b983b507bd6f9f0e4b0ae61a883bc629
Ariana I <git@arianaivan.com>
2025-05-05 18:55:52 -0700

    landing: Tweak intro design

--- app/IntroToSIV.tsx
+++ app/IntroToSIV.tsx
@@ -2,20 +2,24 @@ import { linkStyle } from './page'
 
 export const IntroToSIV = () => {
   return (
-    <div className="text-center space-y-12 mt-4">
-      <p>
+    <div className="text-center space-y-8 text-lg">
+      <p className="sm:text-xl leading-relaxed">
         The{' '}
         <a href="https://siv.org/" target="_blank" className={linkStyle}>
-          Secure Internet Voting Protocol
+          SIV Protocol
         </a>{' '}
-        (SIV) makes voting fast & easy, while ensuring an auditable voter roll,
+        makes voting fast & easy, while ensuring an auditable voter roll,
         end-to-end voter verifiability, and strong privacy.
       </p>
 
-      <p className="border-2 border-[#114852] rounded-lg p-4">
-        Even if the worst actors are in charge of election infrastructure,
-        anyone can verify whether the election was run fairly or not.
-      </p>
+      <div className="relative">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#114852]/20 to-[#114852]/13" />
+        <p className="border-2 border-[#114852] rounded-lg p-6 font-medium bg-black/20">
+          Even if the <b>worst actors</b> are in charge of election
+          infrastructure, anyone can verify whether the election was run fairly
+          or not.
+        </p>
+      </div>
     </div>
   )
 }
